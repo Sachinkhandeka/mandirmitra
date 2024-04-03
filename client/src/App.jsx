2f41a8c2fcc8b867f0d1e1b4aa5005bc39353293
Sachin Khandeka <khandekasachin271@gmail.com>
2024-04-03 10:32:14 +0530

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import {  Button } from "flowbite-react";
+import { GiByzantinTemple } from "react-icons/gi";
+
+export default function App() {
+  return (
+    <>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <GiByzantinTemple size={26} />
+    </>
+  )
+}
