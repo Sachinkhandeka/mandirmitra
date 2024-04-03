864f1d5c8583bd015f564dd2413cce6fec8059d5
Sachin Khandeka <khandekasachin271@gmail.com>
2024-04-03 14:50:54 +0530

    added the basic layout of app

---
+++ client/src/components/DashContent.jsx
@@ -0,0 +1,7 @@
+export default function DashContent() {
+    return(
+        <div>
+            Content
+        </div>
+    );
+}
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,5 +1,43 @@
+import { useState } from "react";
+
+
+import { IoIosArrowDropright, IoIosArrowDropleft } from "react-icons/io";
+
+import DashContent from "../components/DashContent";
+import DashSidebar from "../components/DashSidebar";
+import Header from "../components/Header";
+
+
 export default function Dashboard() {
+    const [showSidebar, setShowSidebar] = useState(false); // State to manage sidebar visibility
+
+    // Function to toggle sidebar visibility
+    const toggleSidebar = () => {
+        setShowSidebar(!showSidebar);
+    };
     return  (
-        <div>Dashboard</div>
+        <>
+            <Header />
+            <div className="flex gap-2">
+                { showSidebar ? (
+                    <div className="fixed h-full w-64 bg-gray-200 overflow-y-auto z-10">
+                        <div className="absolute right-2 top-2" onClick={toggleSidebar} >
+                            { showSidebar ? <IoIosArrowDropleft size={26} className="text-gray-500 cursor-pointer hover:text-black" /> :<IoIosArrowDropright size={26} className="text-gray-500 cursor-pointer hover:text-black"  />  }
+                        </div>
+                        <DashSidebar />
+                    </div>
+                ) : (
+                    <div className="fixed h-full w-10 bg-gray-200 overflow-y-auto z-10">
+                        <div className="absolute right-2 top-2" onClick={toggleSidebar} >
+                            { showSidebar ? <IoIosArrowDropleft size={26} className="text-gray-500 cursor-pointer hover:text-black "  /> : <IoIosArrowDropright size={26} className="text-gray-500 cursor-pointer hover:text-black "  />  }
+                        </div>
+                    </div>
+                ) }
+                <div className={`${showSidebar ? ' ml-64': 'ml-12' } flex-1 overflow-x-auto relative z-0 p-3`}>
+                    <DashContent />
+                </div>
+
+            </div>
+        </>
     );
 }
