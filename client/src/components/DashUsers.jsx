5bfe8778f16f8a73baf64c141acb30a251b643aa
Sachin Khandeka <khandekasachin271@gmail.com>
2024-04-12 17:04:18 +0530

    added users dash for admin

---
+++ client/src/components/DashUsers.jsx
@@ -0,0 +1,90 @@
+import { Table, Toast } from "flowbite-react";
+import { useEffect, useState } from "react";
+import { HiCheck, HiExclamation, HiX } from "react-icons/hi";
+import { useSelector } from "react-redux";
+import { MdOutlineEdit, MdDeleteOutline } from "react-icons/md";
+import { Link } from "react-router-dom";
+
+export  default  function DashUsers() {
+    const { currUser } = useSelector(state => state.user);
+    const [ users, setUsers ] = useState([]);
+    const [ error , setError ] = useState(null);
+    const [ success , setSuccess ] = useState(null);
+
+    //get-fetch all users data
+    const getUsers = async(e)=> {
+        e.preventDefault();
+        try {
+            setError(null);
+            setSuccess(null);
+
+            const response  =  await fetch(`/api/user/get/${currUser.templeId}`);
+            const  data = await response.json();
+
+            if(!response.ok) {
+                return setError(data.message);
+            }
+            setSuccess(data);
+        }catch(err) {
+            setError(err.message);
+        }
+    }
+    useEffect(()=> {
+        getUsers();
+    },[currUser])
+    return (
+        <>
+            { success && (
+                <Toast className="mb-3" >
+                    <div className="inline-flex h-8 w-8 shrink-0 items-center justify-center rounded-lg bg-green-100 text-green-500 dark:bg-green-800 dark:text-green-200">
+                        <HiCheck className="h-5 w-5" />
+                    </div>
+                    <div className="ml-3 text-sm font-normal">{ success }</div>
+                    <Toast.Toggle onDismiss={()=> setSuccess(null)} />
+                </Toast>
+            ) }
+            { error && (
+                <Toast className="mb-3" >
+                    <div className="inline-flex h-8 w-8 shrink-0 items-center justify-center rounded-lg bg-red-100 text-red-500 dark:bg-red-800 dark:text-red-200" >
+                        <HiX className="h-5 w-5" />
+                    </div>
+                    <div className="ml-3 text-sm font-normal" >{ error }</div>
+                    <Toast.Toggle onDismiss={()=> setError(null)} />
+                </Toast>
+            ) }
+            { currUser.isAdmin && ( 
+                <Table striped >
+                    <Table.Head>
+                        <Table.HeadCell>Profile</Table.HeadCell>
+                        <Table.HeadCell>Username</Table.HeadCell>
+                        <Table.HeadCell>Email</Table.HeadCell>
+                        <Table.HeadCell>Roles</Table.HeadCell>
+                        <Table.HeadCell>Actions</Table.HeadCell>
+                    </Table.Head>
+                    {
+                        users && Array.isArray(users) && users.length > 0 && (
+                            users.map((user)=> {
+                                <Table.Body className="divide-y" key={user._id} >
+                                    <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800" >
+                                        <Table.Cell>
+                                            <img src={user.profilePicture} alt={user.username} className="h-10 w-10 rounded-full" />
+                                        </Table.Cell>
+                                        <Table.Cell>{ user.username }</Table.Cell>
+                                        <Table.Cell>{ user.email }</Table.Cell>
+                                        <Table.Cell>{ user.roles }</Table.Cell>
+                                        <Table>
+                                            <div className="flex items-center gap-4" >
+                                                <Link to={"#"} > <MdOutlineEdit size={20} />  </Link>
+                                                <Link to={"#"} > <MdDeleteOutline size={20}/> </Link>
+                                            </div>
+                                        </Table>
+                                    </Table.Row>
+                                </Table.Body>
+                            })
+                        )
+                    }
+                </Table>
+            )}
+        </>
+    )
+}
