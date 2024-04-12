d4d2767ebab59980ae103b549139384ab42fb5aa
Sachin Khandeka <khandekasachin271@gmail.com>
2024-04-12 17:13:31 +0530

    added api route for getting all user with templeId

--- server/routes/user.js
+++ server/routes/user.js
@@ -4,11 +4,18 @@ const user = require("../controllers/userController");
 const wrapAsync = require("../utils/wrapAsync");
 const { verifyAdmin } = require("../utils/verifyAdmin");
 
-//signup route
+//create route
 router.post(
     "/create/:templeId",
     verifyAdmin,
     wrapAsync(user.createController),
+);
+
+//get route
+router.get(
+    "/get/:templeId",
+    verifyAdmin,
+    wrapAsync(user.getController),
 );
 
 module.exports = router ; 
