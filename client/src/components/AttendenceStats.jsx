6cce2c2d1df2cd7222603cd18c8a856cb70b9c34
Sachin Khandeka <khandekasachin271@gmail.com>
2024-06-07 13:15:05 +0530

    showed more info about temple & added attendance on event card

---
+++ client/src/components/AttendenceStats.jsx
@@ -0,0 +1,26 @@
+import { FaUserCheck, FaUserTimes, FaUsers } from 'react-icons/fa';
+
+export default function AttendanceStats({ guestCount, attended, notAttended }) {
+    return (
+        <div className="mt-1 w-full py-4 px-6 bg-white dark:bg-slate-700 rounded-lg shadow-md flex justify-around items-center text-center">
+            <div className="flex flex-col items-center justify-center gap-2">
+                <div className="flex items-center justify-center w-8 h-8 bg-blue-100 rounded-full">
+                    <FaUsers className="text-2xl text-blue-500" size={14} />
+                </div>
+                <p className="text-sm font-bold text-blue-500">{guestCount === 0 ? '00' : guestCount}</p>
+            </div>
+            <div className="flex flex-col items-center justify-center gap-2">
+                <div className="ml-4 flex items-center justify-center w-8 h-8 bg-green-100 rounded-full">
+                    <FaUserCheck className="text-2xl text-green-500" size={14} />
+                </div>
+                <p className="text-sm font-bold text-green-500">{attended === 0 ? '00' : attended}</p>
+            </div>
+            <div className="ml-4 flex flex-col items-center justify-center gap-2">
+                <div className="flex items-center justify-center w-8 h-8 bg-red-100 rounded-full">
+                    <FaUserTimes className="text-2xl text-red-500" size={14} />
+                </div>
+                <p className="text-sm font-bold text-red-500">{notAttended === 0 ? '00' : notAttended}</p>
+            </div>
+        </div>
+    );
+}
---
+++ client/src/components/GodCard.jsx
@@ -0,0 +1,42 @@
+// Function to get a random gradient color
+function getRandomGradient() {
+    const gradients = [
+        "from-blue-300 to-indigo-500",
+        "from-purple-400 to-pink-500",
+        "from-yellow-400 to-orange-500",
+        "from-green-400 to-teal-500",
+        "from-red-400 to-pink-500",
+        "from-indigo-400 to-purple-500",
+        "from-pink-400 to-rose-500",
+        "from-orange-400 to-yellow-500",
+        "from-teal-400 to-cyan-500",
+        "from-rose-400 to-red-500"
+    ];
+    return gradients[Math.floor(Math.random() * gradients.length)];
+}
+
+export default function GodCard({ godsAndGoddesses }) {
+
+    return (
+        <>
+        <h2 className="text-xl font-serif uppercase font-semibold my-4" >Devi-Devtas</h2>
+        <div className="flex overflow-x-auto scrollbar-hidden">
+            <div className="my-2 flex gap-4">
+                {godsAndGoddesses.map((god, index) => (
+                    <div
+                        key={index}
+                        className={`bg-white rounded-lg w-24 md:w-44 shadow-md overflow-hidden transform transition-all duration-300 hover:scale-105`}
+                    >
+                        <div className="w-full h-24 md:h-40 object-cover relative">
+                            <img src={god.image} alt={god.name} className="w-full h-full object-cover" />
+                        </div>
+                        <div className={`h-full text-center px-4 py-2 bg-gradient-to-r ${getRandomGradient()}`}>
+                            <h3 className="text-sm mb-2">{god.name}</h3>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+        </>
+    );
+};
