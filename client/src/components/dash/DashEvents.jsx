d34ef7af747a71d6e05dace0b2381c17dbef0930
Sachin Khandeka <khandekasachin271@gmail.com>
2024-06-20 21:15:24 +0530

    added debouncing at donation,expense and guest

--- client/src/components/dash/DashEvents.jsx
+++ client/src/components/dash/DashEvents.jsx
@@ -1,13 +1,14 @@
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState, useRef, useCallback } from "react";
 import { IoIosArrowForward, IoIosArrowBack } from "react-icons/io";
 import { IoCheckmarkDone } from "react-icons/io5";
-import { TbFaceIdError } from "react-icons/tb"; // Import the error icon
+import { TbFaceIdError } from "react-icons/tb"; 
 import { useSelector } from "react-redux";
 import { Table } from "flowbite-react";
 import EventCard from "../EventCard";
 import ToggleSwitch from "../ToggleSwitch";
 import { useLocation } from "react-router-dom";
 import { Helmet } from 'react-helmet-async';
+import { debounce } from "lodash";
 
 export default function DashEvents() {
     const { currUser } = useSelector(state => state.user);
@@ -60,7 +61,7 @@ export default function DashEvents() {
     // Get invited donors
     const getGuestsData = async () => {
         if (!selectedEventId) return;
-
+        
         const tab = queryParams.get("tab");
 
         // Conditionally include searchTerm only when tab is 'events'
@@ -81,8 +82,11 @@ export default function DashEvents() {
         }
     };
 
+    //debounce getGuestData function 
+    const debouncedFetchGuests = useCallback(debounce(getGuestsData, 500), [selectedEventId, searchTerm]);
+
     useEffect(() => {
-        getGuestsData();
+        debouncedFetchGuests();
     }, [selectedEventId, searchTerm]);
 
     // Handle verification toggle
@@ -105,107 +109,105 @@ export default function DashEvents() {
         }
     };
 
-    // Check for permissions
-    const hasPermission = (actions) => {
-        return currUser.isAdmin || (currUser.roles && currUser.roles.some(role =>
-            role.permissions.some(p => actions.includes(p.actions))
-        ));
-    };
-
     return (
         <>
             <Helmet>
                 <title>Manage Temple Events - Dashboard</title>
                 <meta name="description" content="Manage and track temple events in your dashboard. View, edit, and delete events with ease." />
             </Helmet>
-
-            {events && events.length > 0 ? (
-                <>
-                    {hasPermission(["read", "update", "delete"]) && (
-                        <div className="mb-6 sticky left-0">
-                            <div className="relative">
-                                <IoIosArrowBack
-                                    className="absolute left-0 top-1/2 transform -translate-y-1/2 cursor-pointer border border-slate-500 rounded-full p-2 hover:shadow-lg"
-                                    size={36}
-                                    onClick={scrollLeft}
+            {events && events.length > 0 && (
+                (currUser && currUser.isAdmin) ||
+                (currUser.roles && currUser.roles.some(role =>
+                    role.permissions.some(p =>
+                        p.actions.includes("read") || p.actions.includes("update") || p.actions.includes("delete"))))
+            ) && (
+                <div className="mb-6 sticky left-0">
+                    <div className="relative">
+                        <IoIosArrowBack
+                            className="absolute left-0 top-1/2 transform -translate-y-1/2 cursor-pointer border border-slate-500 rounded-full p-2 hover:shadow-lg"
+                            size={36}
+                            onClick={scrollLeft}
+                        />
+                        <div
+                            ref={scrollRef}
+                            className="flex overflow-x-scroll scrollbar-hidden"
+                        >
+                            {events && events.map(event => (
+                                <EventCard
+                                    key={event._id}
+                                    id={event._id}
+                                    name={event.name}
+                                    date={event.date}
+                                    location={event.location}
+                                    status={event.status}
+                                    setIsEventUpdated={setIsEventUpdated}
+                                    isSelected={selectedEventId === event._id}
+                                    onClick={() => setSelectedEventId(event._id)}
                                 />
-                                <div
-                                    ref={scrollRef}
-                                    className="flex overflow-x-scroll scrollbar-hidden"
-                                >
-                                    {events.map(event => (
-                                        <EventCard
-                                            key={event._id}
-                                            id={event._id}
-                                            name={event.name}
-                                            date={event.date}
-                                            location={event.location}
-                                            status={event.status}
-                                            setIsEventUpdated={setIsEventUpdated}
-                                            isSelected={selectedEventId === event._id}
-                                            onClick={() => setSelectedEventId(event._id)}
-                                        />
-                                    ))}
-                                </div>
-                                <IoIosArrowForward
-                                    className="absolute right-0 top-1/2 transform -translate-y-1/2 cursor-pointer border border-slate-500 rounded-full p-2 hover:shadow-lg"
-                                    size={36}
-                                    onClick={scrollRight}
-                                />
-                            </div>
+                            ))}
                         </div>
-                    )}
-                    {selectedEventId && (
-                        guests.length > 0 ? (
-                            <Table striped>
-                                <Table.Head>
-                                    <Table.HeadCell>Sr.No</Table.HeadCell>
-                                    <Table.HeadCell>Event</Table.HeadCell>
-                                    <Table.HeadCell>Guest Name</Table.HeadCell>
-                                    <Table.HeadCell>PassCode</Table.HeadCell>
-                                    <Table.HeadCell>Invited</Table.HeadCell>
-                                    {hasPermission(["update", "delete"]) && (
-                                        <Table.HeadCell>Verify Guest</Table.HeadCell>
-                                    )}
-                                </Table.Head>
-                                <Table.Body>
-                                    {guests.map((guest, indx) => (
-                                        <Table.Row key={guest._id}>
-                                            <Table.Cell>{indx + 1}</Table.Cell>
-                                            <Table.Cell>{guest.event.name}</Table.Cell>
-                                            <Table.Cell>{guest.donorName}</Table.Cell>
-                                            <Table.Cell>{guest.passCode}</Table.Cell>
-                                            <Table.Cell>{guest.invited === true ? <IoCheckmarkDone size={24} color="green" /> : null}</Table.Cell>
-                                            {hasPermission(["update"]) && (
-                                                <Table.Cell>
-                                                    <ToggleSwitch
-                                                        checked={guest.attended}
-                                                        onChange={(checked) => handleVerificationToggle(guest._id, checked)}
-                                                        isDisabled={guest.event.status === "completed"}
-                                                    />
-                                                </Table.Cell>
-                                            )}
-                                        </Table.Row>
-                                    ))}
-                                </Table.Body>
-                            </Table>
-                        ) : (
-                            <div className="flex justify-center items-center h-screen">
-                                <div className="text-center flex flex-col items-center justify-center">
-                                    <TbFaceIdError size={50} className="animate-bounce" />
-                                    <p>No Guest Invited yet!</p>
-                                </div>
-                            </div>
-                        )
-                    )}
-                </>
-            ) : (
-                <div className="flex justify-center items-center h-screen">
-                    <div className="text-center flex flex-col items-center justify-center">
-                        <TbFaceIdError size={50} className="animate-bounce" />
-                        <p>No Events Created Yet!</p>
+                        <IoIosArrowForward
+                            className="absolute right-0 top-1/2 transform -translate-y-1/2 cursor-pointer border border-slate-500 rounded-full p-2 hover:shadow-lg"
+                            size={36}
+                            onClick={scrollRight}
+                        />
                     </div>
                 </div>
+            )}
+            {selectedEventId && events && events.length > 0 && (
+                (currUser && currUser.isAdmin) ||
+                (currUser.roles && currUser.roles.some(role =>
+                    role.permissions.some(p =>
+                        p.actions.includes("read"))))
+            ) && (
+                guests && guests.length > 0 ? (
+                    <Table striped>
+                        <Table.Head>
+                            <Table.HeadCell>Sr.No</Table.HeadCell>
+                            <Table.HeadCell>Event</Table.HeadCell>
+                            <Table.HeadCell>Guest Name</Table.HeadCell>
+                            <Table.HeadCell>PassCode</Table.HeadCell>
+                            <Table.HeadCell>Invited</Table.HeadCell>
+                            {(currUser && currUser.isAdmin ||
+                                (currUser.roles &&
+                                    currUser.roles.some(role =>
+                                        role.permissions.some(p => p.actions.includes("update") || p.actions.includes("delete")))
+                                )) && <Table.HeadCell>Verify Guest</Table.HeadCell>
+                            }
+                        </Table.Head>
+                        <Table.Body>
+                            {guests.map((guest, indx) => (
+                                <Table.Row key={guest._id}>
+                                    <Table.Cell>{ indx + 1 }</Table.Cell>
+                                    <Table.Cell>{guest.event.name}</Table.Cell>
+                                    <Table.Cell>{guest.donorName}</Table.Cell>
+                                    <Table.Cell>{guest.passCode}</Table.Cell>
+                                    <Table.Cell>{guest.invited === true ? <IoCheckmarkDone  size={24} color="green" /> : null}</Table.Cell>
+                                    {(currUser && currUser.isAdmin ||
+                                        (currUser.roles &&
+                                            currUser.roles.some(role =>
+                                                role.permissions.some(p => p.actions.includes("update")))
+                                        )) && (
+                                            <Table.Cell>
+                                                <ToggleSwitch
+                                                    checked={guest.attended}
+                                                    onChange={(checked) => handleVerificationToggle(guest._id, checked)}
+                                                    isDisabled={guest.event.status === "completed"}
+                                                />
+                                            </Table.Cell>
+                                        )}
+                                </Table.Row>
+                            ))}
+                        </Table.Body>
+                    </Table>
+                ) : (
+                    <div className="flex justify-center items-center h-screen">
+                        <div className="text-center flex flex-col items-center justify-center">
+                            <TbFaceIdError size={50} className="animate-bounce" />
+                            <p>No Guest Invited yet!</p>
+                        </div>
+                    </div>
+                )
             )}
         </>
     );
