fa584b5715622679a96608660c16e3ac49b9b281
Sachin Khandeka <khandekasachin271@gmail.com>
2024-06-25 13:20:10 +0530

    modified the OAuth component

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -39,7 +39,7 @@ export default function OAuth({ templeId }) {
                 dispatch(signinFailure(data.message));
                 return ;
             }
-            dispatch(signinSuccess(data.rest));
+            dispatch(signinSuccess(data.currUser));
             navigate("/");
 
         }catch(err) {
