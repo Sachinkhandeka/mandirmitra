ef7469c88fafd3a076dde6bd531ceebd4b7412bf
Sachin Khandeka <khandekasachin271@gmail.com>
2024-06-29 09:08:58 +0530

    modified firebase configuration and changed layout of home page

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -4,13 +4,15 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "prayasitrack.firebaseapp.com",
-  projectId: "prayasitrack",
-  storageBucket: "prayasitrack.appspot.com",
-  messagingSenderId: "919381319970",
-  appId: "1:919381319970:web:8e8f0668d2fe641bb0857b"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: "mandirmitra-bd32e.firebaseapp.com",
+    projectId: "mandirmitra-bd32e",
+    storageBucket: "mandirmitra-bd32e.appspot.com",
+    messagingSenderId: "1043845713156",
+    appId: "1:1043845713156:web:a9b630be80d86d4c7ed451",
+    measurementId: "G-GMQ8406ZZR"
 };
 
 // Initialize Firebase
