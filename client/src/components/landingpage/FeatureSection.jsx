47c9e143b07f5a775d11fca5bfd77fd8c01c0bb6
Sachin Khandeka <khandekasachin271@gmail.com>
2024-07-08 11:35:51 +0530

    modified landingpage info

--- client/src/components/landingpage/FeatureSection.jsx
+++ client/src/components/landingpage/FeatureSection.jsx
@@ -1,4 +1,4 @@
-import { FaGoogle, FaUser, FaLock, FaCalendarAlt, FaFilePdf, FaUserShield, FaDonate } from 'react-icons/fa';
+import { FaGoogle, FaUser, FaLock, FaCalendarAlt, FaFilePdf, FaUserShield, FaDonate, FaLuggageCart  } from 'react-icons/fa';
 import { FaMoneyBillTrendUp } from "react-icons/fa6";
 import FeatureCard from './FeatureCard';
 
@@ -10,6 +10,7 @@ export  default function FeatureSection() {
                 <FeatureCard icon={<FaDonate />} title="Daan Management" description="Sabhi daanon ka record rakhein aur receipts generate karein." style={"bg-yellow-300"} />
                 <FeatureCard icon={<FaMoneyBillTrendUp />} title="Kharch Ka Hisaab" description="Mandir ke sabhi kharchon ko manage karein." style={"bg-green-300"} />
                 <FeatureCard icon={<FaCalendarAlt />} title="Events Management" description="Sabhi events ka management, passcode ke sath." style={"bg-orange-300"} />
+                <FeatureCard icon={<FaLuggageCart />} title="Inventories Management" description="Mandir ke inventory(Saamagri Prabandhan) items ko track aur manage karein." style={"bg-indigo-300"}/>
                 <FeatureCard icon={<FaFilePdf />} title="Invitation PDF Generation" description="Vishisht donors ke liye personalized invitation PDFs banayein." style={"bg-blue-300"}/>
                 <FeatureCard icon={<FaUser />} title="Devi-Devta Management" description="Apne mandir ke sabhi devi-devtaon ko add karein." style={"bg-red-300"}/>
                 <FeatureCard icon={<FaUserShield />} title="Role-Based Access" description="Alag-alag users ke liye alag-alag access rights." style={"bg-teal-300"}/>
